485396cf7f1eaeb45bbb631fbaf224d03f6e631d
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-10-10 13:44:44 +0200

    Add files via upload

---
+++ 10.10_kontrola_przepływu_sterowania/flowControl.py
@@ -0,0 +1,25 @@
+def funkcjaA():
+	print("t")
+
+def funkcjaB(q):
+	print(q + 1)
+
+def funkcjaC(x)
+	print(x * 2)
+
+b = 4
+c = 7
+x = "a"
+
+print(x)
+funkcjaA()
+funkcjaC(c)
+x = "b"
+
+if b + c > 12:
+	print("b")
+else:
+	print("c")
+print(x)
+funkcjaB(b)
+funkcjaC(b)
