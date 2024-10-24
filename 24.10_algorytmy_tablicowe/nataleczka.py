48e087a1295a050d3cb704d179d3afdfcd5fece2
szaweltomaszek <Pioterzy@gmail.com>
2024-10-24 15:09:33 +0200

    Update nataleczka.py

--- 24.10_algorytmy_tablicowe/nataleczka.py
+++ 24.10_algorytmy_tablicowe/nataleczka.py
@@ -4,7 +4,19 @@ def maluch(dar):
             dar[i], dar[i + 1] = dar[i + 1], dar[i]
     
     return dar
-
+def max_elem(majka):
+	max = majka[0]
+	for i in range(0, 6):
+		if majka[i] > max:
+			max = majka[i]
+	return max
+    
+def max_elem(majka):
+	max = majka[0]
+	for i in range(0, 6):
+		if majka[i] < max:
+			max = majka[i]
+	return max
 t = [3, 5, 2, 9, 1]
 nt = maluch(t)
 print(nt)
