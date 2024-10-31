d7096444870fcb9770ed812f17e2cb05e0fe99ef
GitUser177 <Pala-Gn@zagle.net>
2024-10-31 15:21:07 +0100

    Add files via upload

---
+++ 31.10_konwersja_i_modulo/conversionGP.py
@@ -0,0 +1,71 @@
+
+def get_digit_value(znak):
+    if znak == '0':
+        return 0
+    elif znak == '1':
+        return 1
+    elif znak == '2':
+        return 2
+    elif znak == '3':
+        return 3
+    elif znak == '4':
+        return 4
+    elif znak == '5':
+        return 5
+    elif znak == '6':
+        return 6
+    elif znak == '7':
+        return 7
+    elif znak == '8':
+        return 8
+    elif znak == '9':
+        return 9
+
+def get_char_from_value(cyfra):
+    if cyfra == 0:
+        return '0'
+    elif cyfra == 1:
+        return '1'
+    elif cyfra == 2:
+        return '2'
+    elif cyfra == 3:
+        return '3'
+    elif cyfra == 4:
+        return '4'
+    elif cyfra == 5:
+        return '5'
+    elif cyfra == 6:
+        return '6'
+    elif cyfra == 7:
+        return '7'
+    elif cyfra == 8:
+        return '8'
+    elif cyfra == 9:
+        return '9'
+
+def to_int(napis):
+    liczba = 0
+    dlugosc_napisu = len(napis)
+    for i in range(0, dlugosc_napisu):
+        cyfra = get_digit_value(napis[i])
+        liczba = liczba + cyfra * (10 ** (dlugosc_napisu-i-1))
+    return liczba
+    
+def to_string(liczba):
+    napis = ""
+    
+    for i in range(0, 100):
+        cyfra = liczba%10
+        znak = get_char_from_value(cyfra)
+        napis = znak + napis
+        liczba = (liczba-cyfra)/10
+    return napis
+
+a = input()
+x = to_int(a)
+print(x+1)
+
+y = int(input())
+b = to_string(y)
+print(b)
+
