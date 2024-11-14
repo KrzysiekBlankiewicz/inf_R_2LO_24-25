f8121425edbd6c0a2866e34f9c6eef112e38dc27
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-11-14 13:37:37 +0100

    Add files via upload

---
+++ 14.11_f_kwadratowa/szablon.py
@@ -0,0 +1,50 @@
+
+# wypełnij treścią fragmenty oznaczone #TODO# aby program wypisywał poprawne postaci wzoru funkcji kwadratowej
+# dla ułatwienia możesz założyć, że współczynniki będą liczbami naturalnymi mniejszymi od 10
+
+def wartosc_wielom(params, x):
+    #TODO#
+    return wartosc
+
+def delta(params):
+    #TODO#
+    return d
+    
+def msc_zerowe(params):
+    #TODO#
+    return (x1, x1)
+
+def ilocz(params):
+    #TODO#
+    return (a, p, q)
+
+def kanon(params):
+    #TODO#
+    return (a, x1, x2)
+
+# dane od użytkownika
+print("Podaj wzór funkcji kwadratowej w formacie \"f(x) = ax^2 + bx + c\"")
+funkcja = input()
+
+print("Podaj argument do wyznaczenia wartości funkcji:")
+x = input()
+
+# parametr przy x^2
+p1 = #TODO#
+
+# parametr przy x
+p2 = #TODO#
+
+# wyraz wolny
+p3 = #TODO#
+
+
+# wypisz użytkownikowi wzór funkcji kwadratowej w postaci iloczynowej
+postac_iloczynowa = ilocz(p1, p2, p3)
+a = postac_iloczynowa[0]
+p = postac_iloczynowa[1]
+q = postac_iloczynowa[2]
+print(#TODO#)
+
+# ... i kanonicznej
+#TODO
