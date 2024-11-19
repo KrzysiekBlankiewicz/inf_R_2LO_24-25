0adc0054cbada76065ea2e25b17cc2ea0934b2a2
GitUser177 <Pala-Gn@zagle.net>
2024-11-19 11:20:56 +0100

    Add files via upload

---
+++ 19.11_tablice_dwuwym/PlanszaGP.py
@@ -0,0 +1,30 @@
+
+def drawBoardSimple():
+    print(" ___")
+    print("|   |")
+    print("|   |")
+    print("|___|")
+
+
+def drawBoardComplex(size):
+	first_row = " "
+	for i in range(0,size):
+		first_row += "_"
+	print(first_row)
+
+	for i in range(0, size-2):
+		row = ""
+		for j in range(0, size):
+			row += "|"
+			while(j > 0 and j < size-1):
+				row += " "
+			row += "|"
+		print(row)
+
+	last_row = "|"
+	for i in range(0, size):
+		last_row += "_"
+	last_row += "|"
+	print(last_row)
+
+drawBoardComplex(9)
