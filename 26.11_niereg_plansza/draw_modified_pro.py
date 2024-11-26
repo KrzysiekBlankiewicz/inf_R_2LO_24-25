97c0cc44b3c1d5c3e93de5c84bc27df3f58bcd80
KrzoJan <Krzo-Ja@zagle.net>
2024-11-26 11:24:14 +0100

    Add files via upload

--- 26.11_niereg_plansza/draw_modified_pro.py
+++ 26.11_niereg_plansza/draw_modified_pro.py
@@ -1,34 +1,21 @@
 
-def draw_line(tablica, px, py):
-    ly = 0
+def draw_line(tablica):
     linia = ""
     wewn = False
     for i in tablica:
         if wewn == True:
             linia += "|"
             for j in range(0,i):
-                if j== px and ly == py:
-                    linia += "x|"
-                else:
-                    linia += "_|"
+                linia += "_|"
             wewn = False
             continue
         if wewn == False:
-            for j in range(0, 1):
-                linia += " "
-            for j in range(0,i-1):
+            for j in range(0,i):
                 linia += "  "
             wewn = True
-        ly = ly+1
     print(linia)
 
-tab = [[5,1,4,2], [4,3,1,5], [2,8,1,4], [1,1,1,1,1,], [5,1,4,2]]
-
-poz = [1,1]
-pozx = input("podaj pozycję x")
-pozy = input("podaj pozycję y")
-poz[0] = pozx
-poz[1] = pozy
+tab = [[5,1,4,2], [4,3,1,5], [2,8,1,4], [5,1,4,2]]
 
 for x in tab:
-    draw_line(x, pozx, pozy)
+    draw_line(x)
