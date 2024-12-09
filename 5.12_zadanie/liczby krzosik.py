3f63855a2c1744bdd8ed7af9dc405b81fcbae86d
Иван Иванович <Krzo-Ja@zagle.net>
2024-12-09 23:30:55 +0100

    JAn Krzos odpowiedzi i program
    Zadania z lekcji i te z matury + odpowiedzi do matury

--- 5.12_zadanie/liczby krzosik.py
+++ 5.12_zadanie/liczby krzosik.py
@@ -114,15 +114,25 @@ def czy_współrzędna_z_pierwszych(x,y):
         return false
     if y <= 1:
         return false
+    
     if x > 1:
-        for i in range(2,x):
+        for i in range(2,x-1):
            if (x % i) == 0:
                x_podzielny = True
+              #print("x: jest podzielny")
+              # print(x)
+               break
     if y > 1:
-        for i in range(2,y):
+        for i in range(2,y-1):
            if (y % i) == 0:
                y_podzielny = True
-    if x_podzielny == True and y_podzielny == True:
+               #print("y: jest podzielny")
+               #print(y)
+               break
+               
+    if x_podzielny == True or y_podzielny == True:
+        return "nie"
+    else:
         return "tak"
 
 pierwsze = []
@@ -130,8 +140,132 @@ for i in range(0, len(współrzędne)):
     pierwszość = czy_współrzędna_z_pierwszych(współrzędne[i][0], współrzędne[i][1])
     if pierwszość == "tak":
         pierwsze.append(współrzędne[i])
-    else:
-        break
 print("pary z liczb pierwszych")
 print(pierwsze)
+print("ilość par z liczb pierwszych")
+print(len(pierwsze))
+
+#ZADANIE 4.2
+print("ZADANIE 4.2 ----------------------------------")
+def cyfropodobne(x, y):
+    czy_cyfropodobne = False
+    a = int(len(str(x)))
+    b = int(len(str(y)))
+    #print ("a "+ str(a))
+    #print ("b "+ str(b))
     
+    for i in range (0, a):
+        czy_cyfropodobne = False
+        for j in range (0, b):
+            #print ("porownuje"+ str(x)[i] + " z " +str(y)[j])
+            if int(str(x)[i]) == int(str(y)[j]):
+                czy_cyfropodobne = True
+                #print ("ta sama,przechodze do nastepnej")
+                break
+        if czy_cyfropodobne == False:                    
+            return False
+
+    for i in range (0, b):
+        czy_cyfropodobne = False
+        for j in range (0, a):
+            #print ("porownuje"+ str(x)[i] + " z " +str(y)[j])
+            if int(str(y)[i]) == int(str(x)[j]):
+                czy_cyfropodobne = True
+                #print ("ta sama,przechodze do nastepnej")
+                break
+        if czy_cyfropodobne == False:
+            return False
+        
+    return True
+    
+cyfropodobność = []
+for j in range (0, len(współrzędne)):
+    czy_cyfropodobne = cyfropodobne(współrzędne[j][0], współrzędne[j][1])
+    #print(str(współrzędne[j][0])+" "+str(współrzędne[j][1])+" "+str(czy_cyfropodobne))
+    
+    if czy_cyfropodobne == True:
+        #print(str(współrzędne[j][0])+" "+str(współrzędne[j][1])+" "+str(czy_cyfropodobne))
+        cyfropodobność.append(współrzędne[j])
+print("koniec")
+print(len(cyfropodobność))
+
+#zadanie 4.3
+print("ZADANIE 4.3------------------------------------")
+def odległość(x):
+    naj = 0
+    licznik = 0
+    for n in range(0,len(x)-1):
+        xa = x[n][0]
+        ya = x[n][1]
+        for m in range(n+1,len(x)-1):
+            xb = x[m][0]
+            yb = x[m][1]
+            odległość = (((xb - xa )**2)+((yb - ya)**2))**(1/2)
+            licznik = licznik + 1
+            #if licznik % 10000 ==0:
+                #print(str(licznik) +" ") 
+            if naj < odległość:
+                naj = odległość
+                #print("nowa odległość " + str(naj))
+    return naj
+największa = odległość(współrzędne)
+print(największa)
+print(round(największa))
+
+#zadanie 4.4
+print("ZADANIE 4.4 ------------------------------------------------------")
+def wewnątrz(x):
+    w = []
+    ilosc=0
+    for n in range(0,len(x)):
+        #print ("pozycja: "+ str(x[n][0])+" , "+ str(x[n][1]))
+        if -5000 < x[n][1] < 5000 and -5000 < x[n][0] < 5000:
+            w.append(x[n])
+            ilosc=ilosc+1
+            #print ("jest w srodku")
+    print ("ilosc w srodku: "  +str(ilosc))      
+    return w
+
+def bok(x):
+    w = []
+    ilosc=0
+    for n in range(0,len(x)):
+        if (x[n][1] == 5000 and -5000 <= x[n][0] <= 5000) or ( x[n][0] == 5000 and -5000 <= x[n][1] <= 5000) or (x[n][1] == -5000 and -5000 <= x[n][0] <= 5000) or (x[n][0] == -5000 and -5000 <= x[n][1] <= 5000):
+            w.append(x[n])
+            ilosc=ilosc+1
+    print ("ilosc na linii: "  +str(ilosc))  
+    return w
+
+def poza(x):
+    w = []
+    ilosc=0
+    print ("wielkosc wierszy: "+str(len(x)))
+    for n in range(0,len(x)):
+        print ("jaka jest wartosc n: "+ str(n))
+        if (x[n][1] > 5000 or x[n][1] < -5000 or x[n][1] < -5000 or x[n][0] > 5000):
+            w.append(x[n])
+            ilosc=ilosc+1
+    print ("ilosc na zewnatrz: "  +str(ilosc)) 
+    return w
+
+wewnątrz1 = wewnątrz(współrzędne)
+bok = bok(współrzędne)
+poza = poza(współrzędne)
+
+ilośćw = len(wewnątrz1)
+print("wewnątrz  - " + str(ilośćw))#" + str(wewnątrz1) + "
+ilośćb = len(bok)
+print("bok " + str(bok) + " - " + str(ilośćb))
+ilośćp = len(poza)
+print("poza " + str(ilośćp))#" + str(poza) + " - 
+print ("suma: "+ str(ilośćw+ilośćb+ilośćp))
+print ("wartosc len : "+ str(len(współrzędne)))
+print ("przedostatni wiersz "+str(współrzędne[999]))
+#print ("ostatni wiersz "+str(współrzędne[1000]))
+    
+        
+    
+    
+
+        
+        
