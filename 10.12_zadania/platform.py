d4bce97569e7b1a67ad2e17b26fee9906ae70c71
QuantumEntanglement0 <Pluc-Mi@zagle.net>
2024-12-10 11:21:15 +0100

    M.P 2loa

---
+++ 10.12_zadania/platform.py
@@ -0,0 +1,15 @@
+import math
+
+a1 = int(input())
+a2 = int(input())
+b1 = int(input())
+b2 = int(input())
+
+len1 = a2 - a1 
+len2 = b2 - b1 
+
+if a2 < a1 or b2 < ai:
+   print("Nie")
+else:
+    print(min(a2, b2) - max(a1, b1))
+
