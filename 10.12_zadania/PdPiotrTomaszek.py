e478b373ce4b270b92fcbf2d757b43d5a37e7853
szaweltomaszek <Pioterzy@gmail.com>
2024-12-11 21:09:10 +0100

    Update PdPiotrTomaszek.py

--- 10.12_zadania/PdPiotrTomaszek.py
+++ 10.12_zadania/PdPiotrTomaszek.py
@@ -1,20 +1,24 @@
-def nuda(k,n):
+def nuda(k, n):
     line = ""
     wydruk = 0
     while wydruk < n:
         ile = min(k, n - wydruk)
         line = "$ " * ile
         wydruk += ile
-        k+=2
-        print(line)
+        k += 2
+        print(line) 
     return "Koniec"
 
 print("Wprowadź K:")
 k = int(input())
 print("Wprowadź N:")
 n = int(input())
-if k <= 1 or k >= n or k >= 10^6:
+
+if k <= 1 or k >= n or k >= 10**6:
     print("Zła liczba K")
-if n <= 1 or n <= k or n >= 10^6:
+    exit()
+if n <= 1 or n <= k or n >= 10**6:
     print("Zła liczba N")
-print(nuda(k , n))
+
+print(nuda(k, n))
+
