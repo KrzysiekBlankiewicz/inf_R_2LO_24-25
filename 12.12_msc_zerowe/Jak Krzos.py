6164609121db59464d52d6904371ffdb125437c2
Иван Иванович <Krzo-Ja@zagle.net>
2024-12-16 21:07:41 +0100

    Add files via upload

---
+++ 12.12_msc_zerowe/Jak Krzos.py
@@ -0,0 +1,45 @@
+
+def fkcja(a,b,c,x):
+    return a*(x**2)+(b*x)+c
+
+def msc_zer_v1(a,b,c,p,k,e):
+    srodek = (p+k)/2
+    delta = (b**2) - 4*a*c
+    if delta < 0 or p > k:
+        return "Fajny dowcip"
+    if fkcja(a,b,c,srodek) <= e and fkcja(a,b,c,srodek) >= e:
+        return srodek
+    while abs(fkcja(a,b,c,srodek)) > e:
+        if fkcja(a,b,c,srodek) > 0:
+            k = srodek
+            srodek = (p+k)/2
+        if fkcja(a,b,c,srodek) < 0:
+            p = srodek
+            srodek =(p+k)/2
+    return srodek
+=
+def msc_zer_v2(a,b,c,p,k,e):
+    srodek = (p+k)/2
+    delta = (b**2) - 4*a*c
+    if delta < 0 or p > k:
+        return "Fajny dowcip"
+    if fkcja(a,b,c,srodek) <= e and fkcja(a,b,c,srodek) >= e:
+        return srodek
+    if fkcja(a,b,c,srodek)* fkcja(a,b,c,p) < 0:
+        return msc_zer_v2(a,b,c,p,srodek,e)
+    else:
+        return msc_zer_v2(a,b,c,srodek,k,e)
+
+def fibonaci(n):
+    if n==0 or n == 1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+print("|Miejsce zerowe 1|")
+print(msc_zer_v1(1 , 0 , -1 , 0 , 17, 0.01))
+
+print("|Fibonnachi|")
+print(fibonaci(5))
+##
+print("|Miejsce zerowe 2|")
+print(msc_zer_v1(1 , 4 , -1 , 0 , 17, 0.01))
