c93299cee72301526ff5b53687b005b2fec59a2c
bofron1 <bofron1@gmail.com>
2025-01-14 11:14:03 +0100

    Add files via upload

---
+++ 9.01_szyfrowanie/DeshifratorFull - Franciszek Bodziak.py
@@ -0,0 +1,30 @@
+def cezar_slowo(slowo, n):
+    limit = 122
+    wynik = ""
+    for znak in slowo:
+        if 'a' <= znak <= 'z': 
+            x = ord(znak)
+            x += n
+            if x > limit:
+                x = (x - limit + 96)
+            wynik += chr(x)
+        else:
+            wynik += znak  
+    return wynik
+
+slowo = input("Podaj słowo: ")
+n = 1
+
+syf = cezar_slowo(slowo, n)
+
+with open('slowa.txt', 'r') as plik:
+    linie = [linia.strip() for linia in plik.readlines()]
+
+while True:
+    if syf in linie:
+        print(syf)
+        break
+    else:
+        n += 1
+        syf = cezar_slowo(slowo, n)
+
