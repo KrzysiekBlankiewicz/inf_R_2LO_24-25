c6dbea9b1d54f3f56ae0f966901299f0ca86df4b
szaweltomaszek <Pioterzy@gmail.com>
2025-01-16 15:23:56 +0100

    Add files via upload

---
+++ rsa/sabinka.py
@@ -0,0 +1,20 @@
+import math
+def klucz(p, g):
+    p = 3
+    q = 2
+    n = p*q
+    k == (p-1)(q-1)
+    for i in range(2, k):
+        if math.gcd(i, k) == 1:
+            e = i
+    #(d*e) % k = 1
+    for 
+    priv = (d, n)
+    public = (e, n)
+    return(priv, public)
+
+def zakod(mess, public):
+    e = public[0]
+    n = public[1]
+    
+    
