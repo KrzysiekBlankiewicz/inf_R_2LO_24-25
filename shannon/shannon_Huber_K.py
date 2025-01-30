31cb93394ec397777344421f13070613008a298b
hubiz201 <hubis201@gmail.com>
2025-01-30 15:23:19 +0100

    Add files via upload

---
+++ shannon/shannon_Huber_K.py
@@ -0,0 +1,115 @@
+file = open("tekst.txt")
+
+string = file.read()
+
+suma = 0
+
+for znak in string:
+	suma += 1
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+i=0
+j=0
+k=0
+l=0
+m=0
+n=0
+o=0
+p=0
+q=0
+r=0
+s=0
+t=0
+u=0
+v=0
+w=0
+x=0
+y=0
+z=0
+
+for znak in string:
+	if znak == 'A' or znak == 'a':
+		a += 1
+	if znak == 'B' or znak == 'b':
+		b += 1
+	if znak == 'C' or znak == 'c':
+		c += 1
+	if znak == 'D' or znak == 'd':
+		d += 1
+	if znak == 'E' or znak == 'e':
+		e += 1
+	if znak == 'F' or znak == 'f':
+		f += 1
+	if znak == 'G' or znak == 'g':
+		g += 1
+	if znak == 'H' or znak == 'h':
+		h += 1
+	if znak == 'I' or znak == 'i':
+		i += 1
+	if znak == 'J' or znak == 'j':
+		j += 1
+	if znak == 'K' or znak == 'k':
+		k += 1
+	if znak == 'L' or znak == 'l':
+		l += 1
+	if znak == 'M' or znak == 'm':
+		m += 1
+	if znak == 'N' or znak == 'n':
+		n += 1
+	if znak == 'O' or znak == 'o':
+		o += 1
+	if znak == 'P' or znak == 'p':
+		p += 1
+	if znak == 'Q' or znak == 'q':
+		q += 1
+	if znak == 'R' or znak == 'r':
+		r += 1
+	if znak == 'S' or znak == 's':
+		s += 1
+	if znak == 'T' or znak == 't':
+		t += 1
+	if znak == 'U' or znak == 'u':
+		u += 1
+	if znak == 'V' or znak == 'v':
+		v += 1
+	if znak == 'W' or znak == 'w':
+		w += 1
+	if znak == 'X' or znak == 'x':
+		x += 1
+	if znak == 'Y' or znak == 'y':
+		y += 1
+	if znak == 'Z' or znak == 'z':
+		z += 1
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+print(o)
+print(p)
+print(q)
+print(r)
+print(s)
+print(t)
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
